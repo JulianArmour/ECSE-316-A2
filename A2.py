86bdadfceaffef73480c5f848a6252c03f9b5a71
Julian Armour <julian.armour@videotron.ca>
2020-03-08 15:21:30 -0400

    added 2d fft and inverse fft

--- A2.py
+++ A2.py
@@ -1,28 +1,44 @@
 from cmath import exp, pi
+import numpy as np
 
 
-def slowft(signal):
-    N = len(signal)
-    return [sum([signal[n] * exp(-2j*pi*k*n/N) for n in range(N)]) for k in range(N)]
+def slowft(sig):
+    sig_len = len(sig)
+    return [sum([sig[n] * exp(-2j * pi * k * n / sig_len) for n in range(sig_len)]) for k in range(sig_len)]
 
 
-def fastft(signal):
-    n = len(signal)
+def fastft(sig):
+    n = len(sig)
     if n <= 1:
-        return signal
-    evens = fastft(signal[0::2])
-    odds = fastft(signal[1::2])
+        return sig
+    evens = fastft(sig[0::2])
+    odds = fastft(sig[1::2])
     ws = [exp(-2j * pi * k / n) for k in range(n // 2)]
     return [even + w * odd for even, odd, w in zip(evens, odds, ws)] + \
            [even - w * odd for even, odd, w in zip(evens, odds, ws)]
 
 
-def fastift(signal):
-    n = len(signal)
+def ifastft(sig):
+    n = len(sig)
     if n <= 1:
-        return signal
-    evens = fastft(signal[0::2])
-    odds = fastft(signal[1::2])
+        return sig
+    evens = fastft(sig[0::2])
+    odds = fastft(sig[1::2])
     ws = [exp(2j * pi * k / n) for k in range(n // 2)]
     return [(even + w * odd) / n for even, odd, w in zip(evens, odds, ws)] + \
            [(even - w * odd) / n for even, odd, w in zip(evens, odds, ws)]
+
+
+def fastft2(sig):
+    # perform transform on columns of sig, need to transpose to do so
+    transform1 = [fastft(sigCol) for sigCol in np.transpose(sig)]
+    # perform transform on rows of sig, transpose previous result
+    return [fastft(sigRow) for sigRow in np.transpose(transform1)]
+
+
+def ifastft2(sig):
+    # perform transform on columns of sig, need to transpose to do so
+    transform1 = [ifastft(sigCol) for sigCol in np.transpose(sig)]
+    # perform transform on rows of sig, transpose previous result
+    return [ifastft(sigRow) for sigRow in np.transpose(transform1)]
+
