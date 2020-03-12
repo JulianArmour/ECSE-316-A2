006bb3954a84feda0e2e63ede677b27c22ce9933
Julian Armour <julian.armour@videotron.ca>
2020-03-12 13:51:39 -0400

    fixed bugged inverse fft. made code cleaner

--- A2.py
+++ A2.py
@@ -1,4 +1,5 @@
 from cmath import exp, pi
+import cv2
 import numpy as np
 
 
@@ -13,32 +14,42 @@ def fastft(sig):
         return sig
     evens = fastft(sig[0::2])
     odds = fastft(sig[1::2])
-    ws = [exp(-2j * pi * k / n) for k in range(n // 2)]
-    return [even + w * odd for even, odd, w in zip(evens, odds, ws)] + \
-           [even - w * odd for even, odd, w in zip(evens, odds, ws)]
+    ws = np.exp(-2j * pi * np.arange(n // 2) / n)
+    return np.append(evens + ws * odds, evens - ws * odds)
 
 
 def ifastft(sig):
-    n = len(sig)
-    if n <= 1:
-        return sig
-    evens = fastft(sig[0::2])
-    odds = fastft(sig[1::2])
-    ws = [exp(2j * pi * k / n) for k in range(n // 2)]
-    return [(even + w * odd) / n for even, odd, w in zip(evens, odds, ws)] + \
-           [(even - w * odd) / n for even, odd, w in zip(evens, odds, ws)]
+    """
+    using conjugation method
+    source: https://www.dsprelated.com/showarticle/800.php
+    :param sig: frequency domain numpy array
+    :return: time domain numpy array
+    """
+    return np.conjugate(fastft(np.conjugate(sig))) / len(sig)
 
 
 def fastft2(sig):
-    # perform transform on columns of sig, need to transpose to do so
-    transform1 = [fastft(sigCol) for sigCol in np.transpose(sig)]
-    # perform transform on rows of sig, transpose previous result
-    return [fastft(sigRow) for sigRow in np.transpose(transform1)]
+    def axis_fft(a, axis):
+        return np.apply_along_axis(fastft, axis, a)
+    return np.apply_over_axes(axis_fft, sig, [0, 1])
 
 
 def ifastft2(sig):
-    # perform transform on columns of sig, need to transpose to do so
-    transform1 = [ifastft(sigCol) for sigCol in np.transpose(sig)]
-    # perform transform on rows of sig, transpose previous result
-    return [ifastft(sigRow) for sigRow in np.transpose(transform1)]
+    def axis_ifft(a, axis):
+        return np.apply_along_axis(ifastft, axis, a)
+    return np.apply_over_axes(axis_ifft, sig, [0, 1])
 
+
+def pow2_ceil(num: int) -> int:
+    """
+    :param num: integer to round up
+    :return: num rounded up to the nearest power of 2
+    """
+    return int(np.exp2(np.ceil(np.log2(num))))
+
+
+def pad_image(image):
+    height, width = image.shape
+    x_pad = pow2_ceil(width) - width
+    y_pad = pow2_ceil(height) - height
+    return cv2.copyMakeBorder(image, 0, y_pad, 0, x_pad, cv2.BORDER_CONSTANT, 0)
