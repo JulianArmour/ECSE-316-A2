a64361b832d31f5b0ca59f81b29550d4108639d5
Julian Armour <julian.armour@videotron.ca>
2020-03-14 12:29:47 -0400

    mode one and two

--- A2.py
+++ A2.py
@@ -1,6 +1,8 @@
 from cmath import exp, pi
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import colors as colors
 
 
 def slowft(sig):
@@ -40,7 +42,7 @@ def ifastft2(sig):
     return np.apply_over_axes(axis_ifft, sig, [0, 1])
 
 
-def pow2_ceil(num: int) -> int:
+def pow2_ceil(num):
     """
     :param num: integer to round up
     :return: num rounded up to the nearest power of 2
@@ -53,3 +55,51 @@ def pad_image(image):
     x_pad = pow2_ceil(width) - width
     y_pad = pow2_ceil(height) - height
     return cv2.copyMakeBorder(image, 0, y_pad, 0, x_pad, cv2.BORDER_CONSTANT, 0)
+
+
+def mode1(imgpath='moonlanding.png'):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    image = cv2.imread(imgpath, 0)
+    pimg = pad_image(image)  # padded image
+    freq = np.fft.fft2(pimg)
+    # original image
+    plt.subplot(121)
+    plt.imshow(pimg, cmap='gray')
+    plt.title('Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122)
+    # frequency domain image
+    fimg = plt.imshow(np.abs(freq), norm=colors.LogNorm(vmin=1))
+    plt.title('Frequencies'), plt.xticks([]), plt.yticks([])
+    divider = make_axes_locatable(plt.gca())
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(fimg, cax=cax)
+    plt.show()
+
+
+def mode2(img_path='moonlanding.png', red=0.9):
+    """
+    Filters an image's frequencies using a reduction factor.
+    Example:
+        if red = 0.8 then
+    :param img_path: path to an image file
+    :param red: reduction factor
+    """
+    image = cv2.imread(img_path, 0)
+    ih, iw = image.shape
+    pimg = pad_image(image)  # padded image
+    freq = np.fft.fft2(pimg)
+    frows, fcols = freq.shape
+    r, c = np.ogrid[:frows, :fcols]
+    mask = (r > int((1 - red) * frows)) & (r < red * frows) | (c > int((1 - red) * fcols)) & (c < red * fcols)
+    freq[mask] = 0
+    filtimg = np.real(np.fft.ifft2(freq))[:ih, :iw]
+    plt.subplot(121)
+    plt.imshow(image, cmap='gray'), plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122)
+    plt.imshow(filtimg, cmap='gray'), plt.title('filtered'), plt.xticks([]), plt.yticks([])
+    plt.show()
+    # print number of non-zeros leftover and fraction they represent of original Fourier coefficients
+    non_zeros = int(frows * fcols * (4 * ((1 - red) ** 2)))
+    frac_of_fourier = non_zeros / (frows * fcols)
+    print(f'Number of non-zeros: {non_zeros}')
+    print(f'Fraction of original Fourier: {frac_of_fourier}')
