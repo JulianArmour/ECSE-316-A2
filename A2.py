5582d6ba4d2908058679b2e22200228aed310a0e
Julian Armour <julian.armour@videotron.ca>
2020-04-18 21:26:20 -0400

    fixed bug in mode 3 where images were black

--- A2.py
+++ A2.py
@@ -1,13 +1,11 @@
+import math
+import time
 from cmath import exp, pi
 
 import cv2
 import numpy as np
-import math
-import random
-import time
 from matplotlib import colors as colors
 from matplotlib import pyplot as plt
-from copy import copy, deepcopy
 
 
 def slowft(sig):
@@ -68,7 +66,7 @@ def mode1(imgpath='moonlanding.png'):
     from mpl_toolkits.axes_grid1 import make_axes_locatable
     image = cv2.imread(imgpath, 0)
     pimg = pad_image(image)  # padded image
-    freq = np.fft.fft2(pimg)
+    freq = fastft2(pimg)
     # original image
     plt.subplot(121)
     plt.imshow(pimg, cmap='gray')
@@ -103,7 +101,6 @@ def mode2(img_path='moonlanding.png', red=0.8):
     mask = (r >= int((1 - red) * fcrow)) & (r < (1 + red) * fcrow) | \
            (c >= int((1 - red) * fccol)) & (c < (1 + red) * fccol)
     freq[mask] = 0
-    # freq[int(fcrow * (1 - red)): int(fcrow * (1 + red)), int(fccol * (1 - red)): int(fccol * (1 + red))] = 0
     filtimg = np.real(ifastft2(freq))[:ih, :iw]
     plt.subplot(121)
     plt.imshow(image, cmap='gray'), plt.title('Original image'), plt.xticks([]), plt.yticks([])
@@ -115,46 +112,37 @@ def mode2(img_path='moonlanding.png', red=0.8):
     frac_of_fourier = non_zeros / (f_rows * f_cols)
     print(f'Number of non-zeros: {non_zeros}')
     print(f'Fraction of original Fourier: {frac_of_fourier}')
-    
+
+
 def mode3(img_path='moonlanding.png'):
     image = cv2.imread(img_path, 0)
     ih, iw = image.shape
     pimg = pad_image(image)
     freq = fastft2(pimg)
-    f_rows, f_cols = freq.shape
-    #Compression 0%
-    filtimg0 = np.real(ifastft2(freq))[:ih, :iw]
-    #Take the complex array and make it 1D to be able to sort and get percentile
-    flattend = freq.flatten()
-    flattend.sort()
-    # Get the value of element in the 10% place
-    index10 = math.floor(flattend.size*0.1)
-    index30 = math.floor(flattend.size*0.3)
-    index50 = math.floor(flattend.size*0.5)
-    index75 = math.floor(flattend.size*0.75)
-    index95 = math.floor(flattend.size*0.95)
-    # we will make a deep copy of the original frequency to be able to change the values
-    toCompress10 = freq
-    toCompress30 = freq
-    toCompress50 = freq
-    toCompress75 = freq
-    toCompress95 = freq
+    freq_abs = np.abs(freq)
+    sorted_flat_abs = np.sort(freq_abs.flatten())
+    # Get the value of element in the X% place
+    index10 = math.floor(sorted_flat_abs.size * 0.1)
+    index30 = math.floor(sorted_flat_abs.size * 0.3)
+    index50 = math.floor(sorted_flat_abs.size * 0.5)
+    index75 = math.floor(sorted_flat_abs.size * 0.75)
+    index95 = math.floor(sorted_flat_abs.size * 0.95)
     # If the value is under the threshold, we set it to 0
-    newArray10 = np.where(toCompress10 > flattend[index10], toCompress10, 0)
-    newArray30 = np.where(toCompress30 > flattend[index30], toCompress30, 0)
-    newArray50 = np.where(toCompress50 > flattend[index50], toCompress50, 0)
-    newArray75 = np.where(toCompress75 > flattend[index75], toCompress75, 0)
-    newArray95 = np.where(toCompress95 > flattend[index95], toCompress95, 0)
-    # we take the inverse to get the imge back
-    filtimg10 = np.real(ifastft2(newArray10))[:ih, :iw]
-    filtimg30 = np.real(ifastft2(newArray30))[:ih, :iw]
-    filtimg50 = np.real(ifastft2(newArray50))[:ih, :iw]
-    filtimg75 = np.real(ifastft2(newArray75))[:ih, :iw]
-    filtimg95 = np.real(ifastft2(newArray95))[:ih, :iw]
+    filt10 = np.where(freq_abs > sorted_flat_abs[index10], freq, 0)
+    filt30 = np.where(freq_abs > sorted_flat_abs[index30], freq, 0)
+    filt50 = np.where(freq_abs > sorted_flat_abs[index50], freq, 0)
+    filt75 = np.where(freq_abs > sorted_flat_abs[index75], freq, 0)
+    filt95 = np.where(freq_abs > sorted_flat_abs[index95], freq, 0)
+    # we take the inverse to get the image back
+    filtimg10 = np.real(ifastft2(filt10))[:ih, :iw]
+    filtimg30 = np.real(ifastft2(filt30))[:ih, :iw]
+    filtimg50 = np.real(ifastft2(filt50))[:ih, :iw]
+    filtimg75 = np.real(ifastft2(filt75))[:ih, :iw]
+    filtimg95 = np.real(ifastft2(filt95))[:ih, :iw]
 
-    ##Display images
+    # Display images
     plt.subplot(131)
-    plt.imshow(filtimg0, cmap='gray'), plt.title('Compression 0')
+    plt.imshow(image, cmap='gray'), plt.title('Compression 0')
     plt.subplot(132)
     plt.imshow(filtimg10, cmap='gray'), plt.title('Compression 10')
     plt.subplot(133)
@@ -167,15 +155,16 @@ def mode3(img_path='moonlanding.png'):
     plt.subplot(233)
     plt.imshow(filtimg95, cmap='gray'), plt.title('Compression 95')
     plt.show()
-    
+
     # we calculate how many 0 in the array
-    print("Non zeros with 0% compression: ",np.count_nonzero(freq))
-    print("Non zeros with 10% compression: ",np.count_nonzero(newArray10))
-    print("Non zeros with 30% compression: ",np.count_nonzero(newArray30))
-    print("Non zeros with 50% compression: ",np.count_nonzero(newArray50))
-    print("Non zeros with 75% compression: ",np.count_nonzero(newArray75))
-    print("Non zeros with 95% compression: ",np.count_nonzero(newArray95))
-    
+    print("Non zeros with 0% compression: ", np.count_nonzero(freq))
+    print("Non zeros with 10% compression: ", np.count_nonzero(filt10))
+    print("Non zeros with 30% compression: ", np.count_nonzero(filt30))
+    print("Non zeros with 50% compression: ", np.count_nonzero(filt50))
+    print("Non zeros with 75% compression: ", np.count_nonzero(filt75))
+    print("Non zeros with 95% compression: ", np.count_nonzero(filt95))
+
+
 def mode4():
     array1 = np.random.random(32) + np.random.random(32) * 1j
     complex1 = 32 * 32
@@ -191,125 +180,124 @@ def mode4():
     ##For array1
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         slowft(array1)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0    
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std1 = np.std(list)
-    mean1 = total/10
+    mean1 = total / 10
     ##for array2
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         slowft(array2)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std2 = np.std(list)
-    mean2 = total/10
+    mean2 = total / 10
     ##for array3
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         slowft(array3)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std3 = np.std(list)
-    mean3 = total/10
+    mean3 = total / 10
     ##for array4
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         slowft(array4)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std4 = np.std(list)
-    mean4 = total/10
+    mean4 = total / 10
     ##for array5
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         slowft(array5)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std5 = np.std(list)
-    mean5 = total/10
+    mean5 = total / 10
     ###FASTFTF###
     ##For array6
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         fastft(array1)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0    
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std6 = np.std(list)
-    mean6 = total/10
+    mean6 = total / 10
     ##for array7
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         fastft(array2)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std7 = np.std(list)
-    mean7 = total/10
+    mean7 = total / 10
     ##for array8
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         fastft(array3)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std8 = np.std(list)
-    mean8 = total/10
+    mean8 = total / 10
     ##for array9
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         fastft(array4)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std9 = np.std(list)
-    mean9 = total/10
+    mean9 = total / 10
     ##for array10
     total = 0
     list = []
-    for i in range (10):
+    for i in range(10):
         t0 = time.time()
         fastft(array5)
         t1 = time.time()
-        list.append(t1-t0)
-        total = total + t1-t0
+        list.append(t1 - t0)
+        total = total + t1 - t0
     std10 = np.std(list)
-    mean10 = total/10
-    x=[32,64,256,1024,4096]
-    y=[mean1,mean2,mean3,mean4,mean5]
-    z=[mean6,mean7,mean8,mean9,mean10]
-    plt.errorbar(x, y, yerr=[std1*2,std2*2,std3*2,std4*2,std5*2], fmt='-o')
-    plt.errorbar(x, z, yerr=[std6*2,std7*2,std8*2,std9*2,std10*2], fmt='-o')
+    mean10 = total / 10
+    x = [32, 64, 256, 1024, 4096]
+    y = [mean1, mean2, mean3, mean4, mean5]
+    z = [mean6, mean7, mean8, mean9, mean10]
+    plt.errorbar(x, y, yerr=[std1 * 2, std2 * 2, std3 * 2, std4 * 2, std5 * 2], fmt='-o')
+    plt.errorbar(x, z, yerr=[std6 * 2, std7 * 2, std8 * 2, std9 * 2, std10 * 2], fmt='-o')
     plt.axis([0, 5000, 0, 20])
     plt.suptitle('Algorithm Runtime vs. Problem Size', fontsize=20)
     plt.xlabel('Problem Size ^2', fontsize=18)
     plt.ylabel('Runtime (s)', fontsize=16)
     plt.show()
-       
 
 
 def parse_argv(argv_list):
